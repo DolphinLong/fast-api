fee072ac534a96b081e4b180fdfa0e6becc2a848
DolphinLong <76630845+DolphinLong@users.noreply.github.com>
2021-01-03 02:02:21 +0300

    uptades

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,18 @@ var router = express.Router();
 const users = [];
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(users
-    .sort((user1,user2)=>user1.score <= user2.score)
-    .slice(0,10));
+  res.send(users.sort((user1,user2)=>{
+    if (user1.score<user2){
+      return -1
+    }
+  if (user1.score>user2.score){
+    return 1
+  }
+  return 0
+  ;})
+  .slice(0,10)
+
+    );
 });
 
 router.post('/score', function(req, res, next) {
